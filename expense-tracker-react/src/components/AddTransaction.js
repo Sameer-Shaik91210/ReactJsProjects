77ca11111c8643d6c3a864d535b9ec354bcf3524
SHAIKSAMEER <shaiksameer773006@gmail.com>
2024-12-04 09:21:10 +0530

    finished the expense-tracker-react app

--- expense-tracker-react/src/components/AddTransaction.js
+++ expense-tracker-react/src/components/AddTransaction.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const AddTransaction = () => {
+  const { addTransaction } = useContext(GlobalContext);
+
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text: text,
+      amount: +amount,
+    };
+
+    addTransaction(newTransaction);
+  };
   return (
     <div>
       <h3>Add new transaction</h3>
-      <form id="form">
+      <form id="form" onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
